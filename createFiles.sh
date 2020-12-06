1a8c99eebb2c6ed74b310a25f191068e2a1a2750
i5possible <lianghongbuaa@gmail.com>
2020-12-06 16:44:20 +0800

    add create file template script

---
+++ createFiles.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#define parameters which are passed in.
+Day=$1
+
+#define the template.
+eval "echo \"$(cat  << EOF
+const { readData } = require('./readfile');
+const input = readData('day${Day}Input.txt');
+const lines = input.split(//);
+EOF)\"" > day${Day}.js
+
+eval "echo \"$(cat << EOF
+
+EOF)\"" > day${Day}Input.txt
+
