32cff7d86ce13ed01fb6d87042dbab095f75f044
hong-liang <lianghongbuaa@gmail.com>
2020-12-14 22:54:30 +0800

    add 64 bit operation

--- bitOperation.js
+++ bitOperation.js
@@ -16,6 +16,46 @@ const isolateTheRightmost0Bit = x => ~x & (x+1)
 
 const turnOnTheRightmost0Bit = x => x | (x+1)
 
+const setNthBit64 = (x, n) => {
+    // console.log(x,n)
+    let hi = 0x80000000
+    let lo = 0x7fffffff
+    let high = ~~(x / hi);
+    let low = x & lo;
+    // console.log(`set -- high: ${high}, low: ${low}`);
+    if (n > 31) {
+        return setNthBit(high, n-31) * hi + low;
+    } else if( n == 31) {
+        if (high & 1 === 1) {
+            return x;
+        } else {
+            return (hi * 1)+x;
+        }
+    } else {
+        return high * hi + setNthBit(low, n);
+    }
+}
+
+const unsetNthBit64 = (x, n) => {
+    // console.log(x,n)
+    let hi = 0x80000000
+    let lo = 0x7fffffff
+    let high = ~~(x / hi);
+    let low = x & lo;
+    // console.log(`unset -- high: ${high}, low: ${low}`);
+    if (n > 31) {
+        return unsetNthBit(high, n-31) * hi + low;
+    } else if( n == 31) {
+        if (high & 1 === 1) {
+            return x - hi;
+        } else {
+            return x;
+        }
+    } else {
+        return high * hi + unsetNthBit(low, n);
+    }
+}
+
 module.exports = {
     checkNthBitIsSet,
     setNthBit,
@@ -26,4 +66,6 @@ module.exports = {
     rightPropagateTheRightmost1Bit,
     isolateTheRightmost0Bit,
     turnOnTheRightmost0Bit,
+    setNthBit64,
+    unsetNthBit64,
 }
